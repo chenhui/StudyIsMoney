75f67a76d688d8c65e0dce6dd1c54228442aa40e
chenhui <xiaotian712572@163.com>
2017-11-29 19:26:17 +0800

    finish htmlunit user controller test

--- src/test/java/com/chenhui/controller/UserControllerTest.java
+++ src/test/java/com/chenhui/controller/UserControllerTest.java
@@ -1,19 +1,22 @@
 package com.chenhui.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
-import com.gargoylesoftware.htmlunit.httpclient.HtmlUnitBrowserCompatCookieHeaderValueFormatter;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -21,39 +24,31 @@ public class UserControllerTest {
     
     @Autowired
     private WebClient webClient;
+    
+    private HtmlPage page;
+    
+    @Before
+    public void setup() throws FailingHttpStatusCodeException, MalformedURLException, IOException{
+    	page=this.webClient.getPage("http://localhost:8080/user");
+    }
 
     
 	@Test
 	public void getUserForm() throws Exception {
-		HtmlPage page = this.webClient.getPage("http://localhost:8080/user");
-//		assertThat(page.getBody().getTextContent()).isEqualTo("hello world test my swap!");
 		assertThat(page.getElementById("id")).isExactlyInstanceOf(HtmlTextInput.class);
 		assertThat(page.getElementById("text")).isExactlyInstanceOf(HtmlTextArea.class);
+		assertThat(page.getFormByName("user")).isExactlyInstanceOf(HtmlForm.class);
+	}
+	
+	@Test
+	public void postUserForm() throws Exception{
+		HtmlForm form=page.getFormByName("user");
+		form.getInputByName("id").setValueAttribute("1");
+		form.getTextAreaByName("name").setText("chenhui");
+		HtmlPage listUserPage=form.getInputByName("submitButton").click();
+		assertThat(listUserPage.getBody().getTextContent().contains("id: 1"));
+		assertThat(listUserPage.getBody().getTextContent().contains("name: chenhui"));
 	}
 	
 	
-//    @Before
-//    public void setup() {
-//    	webClient = MockMvcWebClientBuilder
-//    		.webAppContextSetup(context)
-//    		.build();
-//    }
-//    
-//    @Test
-//    public void getUserForm() throws Exception{
-//    	HtmlPage createUserFormPage=webClient.getPage("http://localhost:8080/user");
-//    	HtmlForm form = createUserFormPage.getHtmlElementById("messageForm");
-//    	HtmlTextInput idInput=createUserFormPage.getHtmlElementById("id");
-//    	idInput.setValueAttribute("1");
-//    	HtmlTextArea  nameInput=createUserFormPage.getHtmlElementById("name");
-//    	nameInput.setText("chenhui");
-//    	HtmlSubmitInput submit = form.getOneHtmlElementByAttribute("input", "type", "submit");
-//    	HtmlPage newMessagePage = submit.click();
-//    	
-//    	assertThat(newMessagePage.getUrl().toString()).endsWith("/user");
-//    	assertThat(newMessagePage.getTextContent().contains("Result"));
-//    	assertThat(newMessagePage.getTextContent().contains("id: 0"));
-//    }
-//
-
 }
